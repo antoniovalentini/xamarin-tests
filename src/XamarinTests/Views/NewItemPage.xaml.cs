fd929ff485a6712f6b7ffe8f3adeeb2b4086b3d0
antoniovalentini <antonio.valentini@live.it>
2020-12-19 17:53:34 +0100

    visua studio template

---
+++ src/XamarinTests/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using XamarinTests.Models;
+using XamarinTests.ViewModels;
+
+namespace XamarinTests.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
